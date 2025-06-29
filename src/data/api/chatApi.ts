1a13e6aa66c2f041890c67b92c89cdcbdfa63069
MonsterCT2k3 <monkeyd.nam2003@gmail.com>
2025-06-29 22:45:18 +0700

    update

--- src/data/api/chatApi.ts
+++ src/data/api/chatApi.ts
@@ -10,3 +10,17 @@ export const getConversationsApi = async () => {
         },
     });
 };
+
+export const getMessagesApi = async (conversationId: string, limit: number, before: string) => {
+    let url = `${API_BASE_URL}/chat/message/${conversationId}`;
+    if (limit != 0 && before != "test") {
+        url = `${API_BASE_URL}/chat/message/${conversationId}?limit=${limit}&before=${before}`;
+    }
+    return await authorizedRequest(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+};
+
