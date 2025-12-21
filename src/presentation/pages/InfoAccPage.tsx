8b083b883b9ee6b0934eacf34d6ec3438f79850b
MonsterCT2k3 <monkeyd.nam2003@gmail.com>
2025-12-21 17:16:44 +0700

    nam

--- src/presentation/pages/InfoAccPage.tsx
+++ src/presentation/pages/InfoAccPage.tsx
@@ -14,7 +14,7 @@ import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/AppNavigator';
 import { BACKGROUND } from '@/src/const/constants';
 import PostList from '../components/postList';
-import MemoriesGrid from '../components/memories/Memory';
+import MemoriesGrid from '../components/memories/memory';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState, AppDispatch } from '../redux/store';
 import Toast from 'react-native-toast-message';
